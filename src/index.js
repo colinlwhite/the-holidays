5886253ddd6a46ca9920a5c086ab3e6984fd26ba
Colin White <clwhite814@gmail.com>
2018-11-13 19:52:55 -0600

    google login works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 // import $ from 'jquery';
+import firebase from 'firebase/app';
 import 'bootstrap';
+import apiKeys from '../db/apiKeys.json';
 import createNavbar from './components/Navbar/navbar';
+import loginButton from './components/Auth/auth';
 import './index.scss';
 
 
 const intitializeApp = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
+  loginButton();
 };
 
 intitializeApp();
