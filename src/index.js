83f4eadfe931189eb50114964ea9561501a74cf4
Colin White <clwhite814@gmail.com>
2018-11-19 19:16:11 -0600

    friends are console logging to the console and we are filtering them

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import 'bootstrap';
 import apiKeys from '../db/apiKeys.json';
 import createNavbar from './components/Navbar/navbar';
 import loginButton from './components/Auth/auth';
-import checkLoginStatus from './helpers/authHelpers';
+import authHelpers from './helpers/authHelpers';
+import friendsPage from './components/FriendsPage/friendsPage';
 import './index.scss';
 
 
 const intitializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
-  checkLoginStatus();
+  authHelpers.checkLoginStatus(friendsPage);
   loginButton();
 };
 
